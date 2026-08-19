f58c56946028cb4eeb3b608a6eb508e9f90d4cd2
ROUNAK KUMAR <kumarrounakjha@gmail.com>
2026-08-19 17:47:37 +0530

    image width changed

--- wallMountQRTicketing/src/Components/Carousels.jsx
+++ wallMountQRTicketing/src/Components/Carousels.jsx
@@ -5,16 +5,16 @@ import image3 from '../assets/kmrc_image_3.jpeg'
 function Carousels() {
     return (
         <>
-            <div id="carouselExampleAutoplaying" className="carousel slide p-20" data-bs-ride="carousel">
+            <div id="carouselExampleAutoplaying" className="carousel slide metro-carousel" data-bs-ride="carousel">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <img src={image1} className="d-block w-51 m-auto" alt="Kolkata Metro underwater train" />
+                        <img src={image1} className="d-block metro-carousel-image" alt="Kolkata Metro underwater train" />
                     </div>
                     <div className="carousel-item">
-                        <img src={image2} className="d-block w-50 m-auto" alt="Kolkata Metro train" />
+                        <img src={image2} className="d-block metro-carousel-image" alt="Kolkata Metro train" />
                     </div>
                     <div className="carousel-item">
-                        <img src={image3} className="d-block w-50 m-auto" alt="Kolkata Metro history" />
+                        <img src={image3} className="d-block metro-carousel-image" alt="Kolkata Metro history" />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
