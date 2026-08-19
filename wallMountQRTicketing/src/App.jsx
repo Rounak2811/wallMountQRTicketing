b794fb7f216cef6b0ea99b2bc2d12960ee067d54
ROUNAK KUMAR <kumarrounakjha@gmail.com>
2026-08-19 16:58:59 +0530

    initial commit

---
+++ wallMountQRTicketing/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Carousels from "./Components/Carousels"
+
+function App() {
+  return (
+    <>
+      <h1 className="p-2">Metro Railway Kolkata</h1>
+      <marquee><h2 className="p-3">Welcome to Kolkata Metro - India's First Metro </h2></marquee>
+      <Carousels />
+
+    </>
+  )
+}
+
+export default App
---
+++ wallMountQRTicketing/src/Components/Carousels.jsx
@@ -0,0 +1,33 @@
+import image1 from '../assets/kmrc_image_1.jpg'
+import image2 from '../assets/kmrc_image_2.jpg'
+import image3 from '../assets/kmrc_image_3.jpeg'
+
+function Carousels() {
+    return (
+        <>
+            <div id="carouselExampleAutoplaying" className="carousel slide p-20" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={image1} className=" w-50 m-auto" alt="Kolkata Metro underwater train" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={image2} className=" w-50  h-30 m-auto" alt="Kolkata Metro train" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={image3} className=" w-50 m-auto" alt="Kolkata Metro history" />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </>
+    )
+}
+
+export default Carousels;
