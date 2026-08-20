57774b502ebe90b6b7e9e80d2381b89e061901e1
ROUNAK KUMAR <kumarrounakjha@gmail.com>
2026-08-20 16:51:01 +0530

    added background images

--- wallMountQRTicketing/src/App.jsx
+++ wallMountQRTicketing/src/App.jsx
@@ -5,18 +5,18 @@ import CRISFooter from './Components/CRISFooter';
 
 function App() {
   return (
-    <>
+    <div className="app-container">
       <div className="logoPlusOrg">
         <img src={indian_railway_image} className="railwayLogo" />
         <h1 className="orgName">Metro Railway Kolkata</h1>
       </div>
-      <marquee><h2 className="p-1">Welcome to Kolkata Metro - India's First Metro </h2></marquee>
+      <marquee><h2 className="">Welcome to Kolkata Metro - India's First Metro </h2></marquee>
       <div>
         <Carousels />
-        <button type="button" className="btn btn-primary m-3 p-2 fs-4 rounded">Buy Ticket</button>
+        <button type="button" className="btn btn-primary m-2 p-2 fs-6 rounded-4">Buy Ticket</button>
       </div>
       <CRISFooter />
-    </>
+    </div>
   )
 }
 
